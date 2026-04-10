cbdc9d1b0efaef13cbeaa13a047a8d4f0688a930
Noa-aoN <noa888aon@gmail.com>
2026-04-10 20:12:41 +0900

    Refactor game and result logic

---
+++ js/game-data.js
@@ -0,0 +1,17 @@
+const KebabGameData = {
+  words: [
+    "chicken meat",
+    "cow meat",
+    "sheep meat",
+    "Turkey Meat",
+    "Pita Bread",
+    "anaconda_serpent",
+    "ANACONDA_SERPENT",
+    "dromedaryCamel",
+    "BactrianCamel",
+    "pListData",
+    "iCount",
+    "polka.dot",
+    "pin.dot",
+  ],
+};
